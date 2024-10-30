b30c09feca2aa5d9d75766b23afd9e8969e2e3e1
noja0011 <njaafar@stumail.jccc.edu>
2024-10-30 16:13:55 -0500

    Add files via upload

---
+++ javascript30project1/script.js
@@ -0,0 +1,33 @@
+//Nora Jaafar 10/30/2024
+const secondHand = document.querySelector('.second-hand');
+  const minsHand = document.querySelector('.min-hand');
+  const hourHand = document.querySelector('.hour-hand');
+  const digitalClock = document.getElementById('dig-clock');
+
+  function setDate() {
+    const now = new Date();
+
+   
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+    const hour = now.getHours();
+    const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const secondscount = String(now.getSeconds()).padStart(2, '0');
+    digitalClock.textContent = `${hours}:${minutes}:${secondscount}`;
+  }
+
+  setInterval(setDate, 1000);
+
+  setDate();
+
